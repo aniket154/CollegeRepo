45ac59dfffe0631b31e1fbb985f0dce264721667
aniket154 <daniket154@gmail.com>
2023-07-10 23:36:38 +0530

    save method

--- College/src/main/java/in/bitlogicsystem/main/controller/HomeController.java
+++ College/src/main/java/in/bitlogicsystem/main/controller/HomeController.java
@@ -1,10 +1,21 @@
 package in.bitlogicsystem.main.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import in.bitlogicsystem.main.model.College;
+import in.bitlogicsystem.main.service.CollegeService;
 
 @RestController
 public class HomeController {
 	
+	@Autowired
+	CollegeService collegeservice;
+	
+	public College saveData()
+	{
+		return null;
+	}
 	
 
 }
